3c72d41454cba9ccbcda83ba2e7595892ac20bf9
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-12-23 18:49:41 +0100

    test(core): rename from ConfigurationTests to RunnableConfigTest

--- langgraph4j-core/src/test/java/org/bsc/langgraph4j/RunnableConfigTest.java
+++ langgraph4j-core/src/test/java/org/bsc/langgraph4j/RunnableConfigTest.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ConfigurationTests {
+public class RunnableConfigTest {
 
     @Test
     public void runnableConfigUpdateMetadataTest() {
