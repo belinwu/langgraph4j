a1c82f32da36d8d56660c1e40127e6d31bd0c7cc
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-03-17 00:15:50 +0100

    refactor(MemorySaver): remove lock and loadOrInitCheckpoints method
    work on #360

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/checkpoint/MemorySaver.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/checkpoint/MemorySaver.java
@@ -8,7 +8,6 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class MemorySaver extends AbstractCheckpointSaver {
     private final Map<String, LinkedList<Checkpoint>> _checkpointsByThread = new HashMap<>();
-    private final ReentrantLock _lock = new ReentrantLock();
 
     protected final Map<String, LinkedList<Checkpoint>> cache() {
         return Map.copyOf(_checkpointsByThread);
@@ -34,17 +33,6 @@ public class MemorySaver extends AbstractCheckpointSaver {
 
         return _checkpointsByThread.computeIfAbsent(threadId, k -> new LinkedList<>());
 
-    }
-
-    @Override
-    protected final <T> T loadOrInitCheckpoints(RunnableConfig config,
-                                                TryFunction<LinkedList<Checkpoint>, T, Exception> transformer) throws Exception {
-        _lock.lock();
-        try {
-            return super.loadOrInitCheckpoints(config, transformer);
-        } finally {
-            _lock.unlock();
-        }
     }
 
 }
